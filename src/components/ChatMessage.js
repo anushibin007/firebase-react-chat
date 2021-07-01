26e9c78a72d778ae5ea33143458cac040d1b90e9
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2021-07-01 12:36:21 +0530

    You can now see message time by hovering over the message text

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -9,7 +9,7 @@ const ChatMessage = (props) => {
 	const image = <Image roundedCircle className="avatar" src={message.photoUrl} alt={`Profile picture of ${message.displayName}`} title={message.displayName} />;
 	return (
 		<React.Fragment>
-			<p className={`${from} animate__bounceIn animate__animated`}>
+			<p className={`${from} animate__bounceIn animate__animated`} title={message.createdAt.toDate()}>
 				{from === "from-them" ? image : ""}&nbsp;
 				{message.text}&nbsp;
 				{from === "from-me" ? image : ""}
